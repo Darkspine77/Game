abf787683b1c801bcdbe6d98e17d4c0cf7a681f7
Darkspine77 <Jwoods@Riverdale.edu>
2016-08-18 14:56:23 -0400

    JSON Test

--- js/logCheck.js
+++ js/logCheck.js
@@ -21,22 +21,11 @@
             var providerData = user.providerData;
             user.getToken().then(function(accessToken) {
               document.getElementById('sign-in-status').textContent = 'Signed in';
-              document.getElementById('sign-in').textContent = 'Sign out';
-              document.getElementById('account-details').textContent = JSON.stringify({
-                displayName: displayName,
-                email: email,
-                emailVerified: emailVerified,
-                photoURL: photoURL,
-                uid: uid,
-                accessToken: accessToken,
-                providerData: providerData
-              }, null, '  ');
+              console.log(JSON)
             });
           } else {
             // User is signed out.
             document.getElementById('sign-in-status').textContent = 'Signed out';
-            document.getElementById('sign-in').textContent = 'Sign in';
-            document.getElementById('account-details').textContent = 'null';
           }
         }, function(error) {
           console.log(error);
