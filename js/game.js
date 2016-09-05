d5e557d18cf7ec13640baa13ace08a07ef4b745c
Darkspine77 <Jwoods@Riverdale.edu>
2016-09-05 16:01:19 -0400

    auth test #12

--- js/game.js
+++ js/game.js
@@ -19,6 +19,27 @@ totalExp = 0
   // Initialize Firebase
   var ref = firebase.auth()
   console.log(ref)
+  ref.onAuth(function(authData) {
+  if (authData && isNewUser) {
+    // save the user's profile into the database so we can list users,
+    // use them in Security and Firebase Rules, and show profiles
+    ref.child("users").child(authData.uid).set({
+      provider: authData.provider,
+      name: getName(authData)
+    });
+  }
+});
+// find a suitable name based on the meta info given by each provider
+function getName(authData) {
+  switch(authData.provider) {
+     case 'password':
+       return authData.password.email.replace(/@.*/, '');
+     case 'twitter':
+       return authData.twitter.displayName;
+     case 'facebook':
+       return authData.facebook.displayName;
+  }
+}
 
 
 function setup(){   // Hide
