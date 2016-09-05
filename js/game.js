fe025179d2a387060d9110195685de8d51da9e0b
Darkspine77 <Jwoods@Riverdale.edu>
2016-09-05 15:59:02 -0400

    firebase check #2

--- js/game.js
+++ js/game.js
@@ -21,14 +21,6 @@ totalExp = 0
 
 function setup(){   // Hide
 
-  var config = {
-    apiKey: "AIzaSyDD-AmEnz70-xxCoQwX0f0D6jn0BH4es08",
-    authDomain: "lone-solider.firebaseapp.com",
-    databaseURL: "https://lone-solider.firebaseio.com",
-    storageBucket: "lone-solider.appspot.com",
-  };
-  firebase.initializeApp(config);
-
   var ref = firebase.auth()
   console.log(ref)
 
